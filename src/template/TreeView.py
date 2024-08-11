ade0ec2010f4e6494051ca3908e88ba3049e5d78
Kiwi-Co <kevinbinoj14@gmail.com>
2024-08-11 22:35:00 +0530

    -remove self.root from SignUp and FrontPage -add TreeView as a super class for Flights and Cart

---
+++ src/template/TreeView.py
@@ -0,0 +1,67 @@
+import customtkinter as ctk
+import tkinter as tk
+from tkinter import ttk
+
+class TreeView(ctk.CTkFrame):
+    def __init__(self, root, heading, columns):
+        super().__init__(root)
+
+        self.root = root 
+        self.heading = heading
+        self.columns = columns
+
+        self.row_count = 0
+
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(0,weight=1)
+
+        self.style = ttk.Style(self)
+        self.style.theme_use("clam")
+        self.style.configure("Treeview.Heading", background='#000000', foreground='white', relief='flat', rowheight=50)
+        self.style.configure("Treeview", background='#333333', foreground='white', fieldbackground='#333333', rowheight=45)
+        self.style.map('Treeview', background=[('selected','#D3D3D3')])
+        
+        self.header = ctk.CTkLabel(self, text=self.heading, font=ctk.CTkFont(size=30, weight="bold"))
+        self.header.grid(row=0, column=0, sticky='new')
+
+        self.tree = ttk.Treeview(self, columns=self.columns, show='headings')
+
+        for column in self.columns:
+            self.tree.heading(column, text=column)
+            self.tree.column(column, anchor='center', width=50)
+
+        self.tree.tag_configure('oddrow', background='#333333')
+        self.tree.tag_configure('evenrow', background='#1c1c1c')
+        
+        self.tree.bind('<<TreeviewSelect>>')
+        self.tree.grid(row=1, column=0, sticky='nesw')
+
+        self.scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.tree.yview)
+        self.tree.configure(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.grid(row=1, column=1, sticky='ns')
+
+    def insertRow(self, row):
+        if self.row_count % 2 == 0:
+            self.tree.insert('', tk.END, values=row, tags=('oddrow',)) # pyright: ignore
+        else: 
+            self.tree.insert('', tk.END, values=row, tags=('evenrow',)) # pyright: ignore
+
+        self.row_count += 1
+    
+    def deleteRow(self):
+        selected = self.tree.selection()[0]
+        self.tree.delete(selected)
+
+    def deleteAllRows(self):
+        for row in self.tree.get_children():
+            self.tree.delete(row)
+
+        self.row_count = 0
+
+    def getSelectedRow(self):
+        selected = self.tree.selection()
+        if not selected:
+            return None
+        details = self.tree.item(selected[0])
+        return details["values"]
+
--- src/ui/Flights.py
+++ src/ui/Flights.py
@@ -8,60 +8,18 @@ from ui.AddFlight import AddFlight
 from var.Globals import get_user_role
 from CTkMessagebox import CTkMessagebox as ctkmsgbox
 from datetime import datetime
+from template.TreeView import TreeView
 
 logger = Logger(__name__).logger
 
-class Flights(ctk.CTkFrame):
+class Flights(TreeView):
     def __init__(self, root):
-        super().__init__(root)
+        super().__init__(root, columns=('ID', 'Airline', 'Place of Departure', 'Destination', 'Class', 'Date', 'Time', 'Price'), heading="Available Flights")
 
-        self.root = root 
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_columnconfigure(0,weight=1)
-
-        self.style = ttk.Style(self)
-        self.style.theme_use("clam")
-        self.style.configure("Treeview.Heading", background='#000000', foreground='white', relief='flat', rowheight=50)
-        self.style.configure("Treeview", background='#333333', foreground='white', fieldbackground='#333333', rowheight=45)
-        self.style.map('Treeview', background=[('selected','#D3D3D3')])
-        
-        self.header = ctk.CTkLabel(self, text="Available Flights", font=ctk.CTkFont(size=30, weight="bold"))
-        self.header.grid(row=0, column=0, sticky='new')
-
-        self.columns = ('id', 'airline', 'pod', 'dest', 'class', 'date', 'time', 'price')
-        self.tree = ttk.Treeview(self, columns=self.columns, show='headings')
-        self.tree.heading('id', text='ID')
-        self.tree.column('id', anchor='center', width=50)
-        self.tree.heading('airline', text='Airline')
-        self.tree.column('airline',anchor='center', width=50)
-        self.tree.heading('pod', text='Place of Departure')
-        self.tree.column('pod',anchor='center', width=50)
-        self.tree.heading('dest', text='Destination')
-        self.tree.column('dest',anchor='center', width=50)
-        self.tree.heading('class', text='Class')
-        self.tree.column('class',anchor='center', width=50)
-        
-        self.tree.heading('date', text='Date')
-        self.tree.column('date',anchor='center', width=50)
-        
-        self.tree.heading('time', text='Time')
-        self.tree.column('time',anchor='center', width=50)
-        self.tree.heading('price', text='Price')
-        self.tree.column('price',anchor='center', width=50)
-
+        # example flights
         # ('Indigo','Delhi','Mumbai','Economy', '17:00 - 19:30' ,'5000INR')
         # ('Indigo','Bangalore','Mumbai','Economy', '18:00 - 20:00', '5500INR')
         # ('Emirates','Bangalore','Dubai','First Class', '01:00 - 04:30', '10000INR')
-        
-        self.tree.tag_configure('oddrow', background='#333333')
-        self.tree.tag_configure('evenrow', background='#1c1c1c')
-        
-        self.tree.bind('<<TreeviewSelect>>')
-        self.tree.grid(row=1, column=0, sticky='nesw')
-
-        self.scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.tree.yview)
-        self.tree.configure(yscrollcommand=self.scrollbar.set)
-        self.scrollbar.grid(row=1, column=1, sticky='ns')
 
         self.btn_frame = ctk.CTkFrame(self,fg_color='transparent')
         self.btn_frame.grid(row=2,column=0, sticky='se')
@@ -103,29 +61,36 @@ class Flights(ctk.CTkFrame):
         self.add_flight_form.after(100, self.add_flight_form.lift) #pyright: ignore # work around to fix bug: toplevel hiding behind root
 
     def deleteFlight(self):
-        selected = self.getSelectedFlight()
+        selected = self.getSelectedRow()
         if selected is None:
             ctkmsgbox(title="Flights", message="No flight selected!")
             return
-        if self.deleteRowFlights(selected[0]) is False:
-            return
-        selected = self.tree.selection()[0]
-        self.tree.delete(selected)
+        flight_id = selected[0]
+        sql_cmd_del_passengers = f"DELETE FROM Passengers WHERE Flight_ID = {flight_id}"
+        sql_cmd_del_flights = f"DELETE FROM Flights WHERE Flight_ID = {flight_id};"
+        result_passengers = mysql.execute(sql_cmd_del_passengers)
+        result_flights = mysql.execute(sql_cmd_del_flights)
+
+        if result_passengers[0] is False:
+            logger.error("Failed to delete data from Passengers!")
+            logger.error(result_passengers[1])
+            return 
         
-    def deleteAllRows(self):
-        for row in self.tree.get_children():
-            self.tree.delete(row)
+        if result_flights[0] is False:
+            logger.error("Failed to delete data from Flights!")
+            logger.error(result_flights[1])
+            return 
 
-    def getSelectedFlight(self):
-        selected = self.tree.selection()
-        if not selected:
-            return None
-        details = self.tree.item(selected[0])
-        return details["values"]
+        self.refresh()
 
+        logger.info(f"{get_user_role[appdata.data["user"]["permission"]]}: {appdata.data["user"]["name"]} deleted flight with id {flight_id}")
+        self.root.reinitFrame("Cart")
+        self.root.showFrame("Flights")
+        
     def refresh(self):
         logger.info("Refreshing Flights!")
         self.extractFlights()
+        self.reloadTable()
 
     def extractFlights(self):
         date, time = str(datetime.now()).split()
@@ -138,16 +103,14 @@ class Flights(ctk.CTkFrame):
             logger.error(result[1])
             return
 
-        logger.info("Extracted data from flights")
         self.flights = self.formatFlights(result[1])
+        logger.info("Extracted data from flights")
 
+    def reloadTable(self):
         self.deleteAllRows()
 
-        for count, flight in enumerate(self.flights):
-            if count % 2 == 0:
-                self.tree.insert('', tk.END, values=flight, tags=('oddrow',)) # pyright: ignore
-            else: 
-                self.tree.insert('', tk.END, values=flight, tags=('evenrow',)) # pyright: ignore
+        for flight in self.flights:
+            self.insertRow(flight)
 
     def insertRowFlights(self, row):
         sql_cmd = f"INSERT INTO Flights {str(tuple(row.keys())).replace('\'', '')} VALUES {str(tuple(row.values()))};"
@@ -156,39 +119,17 @@ class Flights(ctk.CTkFrame):
         if result[0] is False:
             logger.error("Failed to insert data to Flights!")
             logger.error(result[1])
-            return False
+            return
+        
+        self.refresh()
 
         logger.info(f"{get_user_role[appdata.data["user"]["permission"]]}: {appdata.data["user"]["name"]} inserted {row} to Flights")
         if self.isAddFlightFormAlive():
             self.add_flight_form.destroy() # pyright: ignore
-            self.add_flight_form.update() # pyright: ignore
-
-        self.extractFlights()
-        return True
-
-    def deleteRowFlights(self, flight_id):
-        sql_cmd_del_passengers = f"DELETE FROM Passengers WHERE Flight_ID = {flight_id}"
-        sql_cmd_del_flights = f"DELETE FROM Flights WHERE Flight_ID = {flight_id};"
-        result_passengers = mysql.execute(sql_cmd_del_passengers)
-        result_flights = mysql.execute(sql_cmd_del_flights)
-
-        if result_passengers[0] is False:
-            logger.error("Failed to delete data from Passengers!")
-            logger.error(result_passengers[1])
-            return False
+            self.add_flight_form.update() # pyright: ignore        
         
-        if result_flights[0] is False:
-            logger.error("Failed to delete data from Flights!")
-            logger.error(result_flights[1])
-            return False
-
-        logger.info(f"{get_user_role[appdata.data["user"]["permission"]]}: {appdata.data["user"]["name"]} deleted flight with id {flight_id}")
-        self.root.reinitFrame("Cart")
-        self.root.showFrame("Flights")
-        return True
-    
     def BookFlight(self):
-        flight_id = self.getSelectedFlight()
+        flight_id = self.getSelectedRow()
         if flight_id is None:
             ctkmsgbox(title="Flights", message="No flight selected!")
             return
